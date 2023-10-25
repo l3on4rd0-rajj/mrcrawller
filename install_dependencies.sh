0ddd4c95905dd49d848d7c0df899f682e3da4488
radesh <98270993+l3on4rd0-rajj@users.noreply.github.com>
2023-10-25 00:07:43 -0300

    Add files via upload

---
+++ install_dependencies.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Atualiza o pip
+python -m pip install --upgrade pip
+
+# Instala as dependências do projeto
+pip install -r requirements.txt
+
+echo "Instalação das dependências concluída."
