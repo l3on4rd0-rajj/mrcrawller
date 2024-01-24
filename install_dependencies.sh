fb91555cad079659d8887714ca81cefae7be81ad
radesh <98270993+l3on4rd0-rajj@users.noreply.github.com>
2024-01-24 18:05:55 -0300

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,7 +1,11 @@
-#!/bin/bash
-
 # Atualiza o pip
 python -m pip install --upgrade pip
+
+# Exibe a versão do pip
+pip --version
+
+# Exibe o conteúdo de requirements.txt
+cat requirements.txt
 
 # Instala as dependências do projeto
 pip install -r requirements.txt
